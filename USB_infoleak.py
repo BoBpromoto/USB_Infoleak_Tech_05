3278518dc286d46eaa6d56929084828f3b709309
L3ad0XFF <L3ad0xff@gmail.com>
2018-02-25 05:07:09 +0900

    link ~ing

--- USB_infoleak.py
+++ USB_infoleak.py
@@ -7,7 +7,9 @@ import sys
 import os
 import platform
 import pyevtx
-from xml.etree.ElementTree import parse
+import csv
+import datetime
+from linkfile import *
 
 # ==> * Registry Parsing Area *
 def D_classes() : 
@@ -245,7 +247,7 @@ def VolumeName() :
 # ==> * Find Install time from setupapi.dev.log Area *
 def setupdevlog() :
 	global initcontime, recentcontime
-	initcontime = list()
+	initcontime = {}
 	recentcontime = list()
 	filepath = "C:\Windows\INF\setupapi.dev.log"
 	file = open(filepath, 'r')
@@ -253,78 +255,163 @@ def setupdevlog() :
 	for i in range (0, len(search4log)) :
 		for j in range (0, len(contents)) :
 			if (search4log[i].split(" : ")[1] in contents[j] and "Install" in contents[j] and "start" in contents[j+1]) :
-				initcontime.append(search4log[i].split("\\")[1] + " : " + contents[j+1].split("start ")[1].replace("\n",""))
-	initcontime = list(set(initcontime))
+				initcontime = {search4log[i].split("\\")[1] : contents[j+1].split("start ")[1].replace("\n","").replace("/", "-")}
 	file.close()
 
 # ==> * EventLog Microsoft-Windows-DriverFrameworks-UserMode%4Operational.evtx Parsing Area *
 def xml_parser(xml_data) :
 	if (xml_data[xml_data.find('<EventID>'):xml_data.find('</EventID>')][-4:] == '2003') :
+		event_val = {}
 		evtx_System = xml_data[xml_data.find('<System>') : xml_data.find('</System>')] # event_id, time, userid
 		evtx_UserData = xml_data[xml_data.find('<UserData>') : xml_data.find('</UserData>')] # device name, lifetime
 
-		print ("Event ID : " + xml_data[xml_data.find('<EventID>'):xml_data.find('</EventID>')][-4:])
-		print ("Log Report Time : " + evtx_System[evtx_System.find('<TimeCreated SystemTime='):evtx_System.find('Z"/>')].split('="')[1].replace("T"," "))
-		print ("User ID : " + evtx_System[evtx_System.find('<Security UserID='):].split('"')[1])
-		print ("Device Name : " + evtx_UserData[evtx_UserData.find('instance='):].split("USBSTOR")[1].split("#")[1])
-		print ("Device Serial_Number : " + evtx_UserData[evtx_UserData.find('instance='):].split("USBSTOR")[1].split("#")[2].split("&")[1])
-		print ("lifetime : " + evtx_UserData[evtx_UserData.find('lifetime='):].split('"')[1] + "\n")
+		e_id = xml_data[xml_data.find('<EventID>'):xml_data.find('</EventID>')][-4:]
+		e_logtime = evtx_System[evtx_System.find('<TimeCreated SystemTime='):evtx_System.find('Z"/>')].split('="')[1].replace("T"," ")
+		e_uid = evtx_System[evtx_System.find('<Security UserID='):].split('"')[1]
+		e_device_name = evtx_UserData[evtx_UserData.find('instance='):].split("USBSTOR")[1].split("#")[1]
+		e_serial = evtx_UserData[evtx_UserData.find('instance='):].split("USBSTOR")[1].split("#")[2].split("&")[1]
+		e_liftime = evtx_UserData[evtx_UserData.find('lifetime='):].split('"')[1]
+
+		event_val = {"Event_ID" : e_id, "Log_Time" : e_logtime, "User_ID" : e_uid, "Device_Name" : e_device_name,
+		"Serial_Number" : e_serial, "Lifetime" : e_liftime}
+		# print (event_val, end=" "); print ("\n")
+		return event_val
 
 	elif (xml_data[xml_data.find('<EventID>'):xml_data.find('</EventID>')][-4:] == '2100') :
+		event_val = {}
 		evtx_System = xml_data[xml_data.find('<System>') : xml_data.find('</System>')] # event_id, time, userid
 		evtx_UserData = xml_data[xml_data.find('<UserData>') : xml_data.find('</UserData>')] # device name, lifetime
 
-		print ("Event ID : " + xml_data[xml_data.find('<EventID>'):xml_data.find('</EventID>')][-4:])
-		print ("Log Report Time : " + evtx_System[evtx_System.find('<TimeCreated SystemTime='):evtx_System.find('Z"/>')].split('="')[1].replace("T"," "))
-		print ("User ID : " + evtx_System[evtx_System.find('<Security UserID='):].split('"')[1])
-		print ("Device Name : " + evtx_UserData[evtx_UserData.find('instance='):].split("USBSTOR")[1].split("#")[1])
-		print ("Device Serial_Number : " + evtx_UserData[evtx_UserData.find('instance='):].split("USBSTOR")[1].split("#")[2].split("&")[1])
-		print ("lifetime : " + evtx_UserData[evtx_UserData.find('lifetime='):].split('"')[1] + "\n")
+		e_id = xml_data[xml_data.find('<EventID>'):xml_data.find('</EventID>')][-4:]
+		e_logtime = evtx_System[evtx_System.find('<TimeCreated SystemTime='):evtx_System.find('Z"/>')].split('="')[1].replace("T"," ")
+		e_uid = evtx_System[evtx_System.find('<Security UserID='):].split('"')[1]
+		e_device_name = evtx_UserData[evtx_UserData.find('instance='):].split("USBSTOR")[1].split("#")[1]
+		e_serial = evtx_UserData[evtx_UserData.find('instance='):].split("USBSTOR")[1].split("#")[2].split("&")[1]
+		e_liftime = evtx_UserData[evtx_UserData.find('lifetime='):].split('"')[1]
+
+		event_val = {"Event_ID" : e_id, "Log_Time" : e_logtime, "User_ID" : e_uid, "Device_Name" : e_device_name,
+		"Serial_Number" : e_serial, "Lifetime" : e_liftime}
+		return event_val
+		# print (event_val, end =" "); print("\n")
+	else :
+		return 0
+
 
 def Eventlogload() : # https://github.com/libyal/libevtx/wiki/Development
+	global file, csvWriter
 	file_object = open("C:\Windows\System32\winevt\Logs\Microsoft-Windows-DriverFrameworks-UserMode%4Operational.evtx", "rb")
 	evtx_file = pyevtx.file()
 	evtx_file.open_file_object(file_object)
-	filename = "temp.xml"
+	file = open("./regi_evtx.csv", 'w', newline = "\n")
+	csvWriter = csv.writer(file)        
+	csvWriter.writerow(['ControlSet', 'Device_Name', 'Serial_Number', 'Mounted_Volume_Name', 'Logical_Drive', 'Init Connected Time', 'Connected Time',
+		'Disconnected Time', 'LifeTime'])
 
 	# help(pyevtx.file) # How to use pyevtx
-	print ("\n================== * EventLog Contents * ==================")
+	# print ("\n================== * EventLog Contents * ==================")
 	for event_cnt in range (0, evtx_file.number_of_records) :
 		record = evtx_file.get_record(event_cnt)
-		# print (record.xml_string)
-		xml_parser(record.xml_string)
-
+		usb_event_log = xml_parser(record.xml_string)
+		if usb_event_log != 0 :
+			# print (usb_event_log, end=" "); print("\n")
+			r_s_e_csv(usb_event_log)
+		else :
+			pass
+	file.close()
 	evtx_file.close()
 
+def regi2dic() :
+	global usb_info
+
+	usb_info = {}
+	for j in range (0, len(controlSetinfo)) :
+		device_list = list()
+		stor_device_list = list()
+		serial_list = list()
+		v_p_list = list()
+		logic_drive_list = list()
+		volumename_list = list()
+		for i in range (0, len(d_class_id)) :
+			if controlSetinfo[j] == d_class_id[i].split(" : ")[0] :
+				device_list.append(d_class_id[i].split(" : ")[1])
+				stor_device_list.append(stor_d_name[i].split(" : ")[1])
+				serial_list.append(d_unique_instance_id[i].split(" : ")[1])
+				v_p_list.append(v_id_p_id[i].split(" : ")[1])
+				logic_drive_list.append(logic_drive_name[i].split(" : ")[1])
+				volumename_list.append(win7_Volname[i].split(" : ")[1])
+
+				device_list = list(set(device_list))
+				stor_device_list = list(set(stor_device_list))
+				serial_list = list(set(serial_list))
+				v_p_list = list(set(v_p_list))
+				logic_drive_list = list(set(logic_drive_list))
+				volumename_list = list(set(volumename_list))
+
+				usb_info[controlSetinfo[j]] = {"Device_Name" : device_list	, "USBSTOR_Device_Name" : stor_device_list,
+				"Serial_Number" : serial_list, "Vander & Product" :v_p_list,
+				"Logical_Drive" : logic_drive_list, "Mounted_Volume_Name" : volumename_list}
+
+	for j in range (0, len(controlSetinfo)) :
+		initcon_list = list()
+		for i in range (0, len(usb_info[controlSetinfo[j]]["USBSTOR_Device_Name"])) :
+			if usb_info[controlSetinfo[j]]["USBSTOR_Device_Name"][i] in initcontime.keys() :
+				initcon_list.append(initcontime[usb_info[controlSetinfo[j]]["USBSTOR_Device_Name"][i]])
+				initcon_list = list(set(initcon_list))
+				usb_info[controlSetinfo[j]]["Init Connect Time"] = initcon_list
+
+def r_s_e_csv(events) :
+	# print (events, end=" "); print("\n")
+	if events["Event_ID"] == '2003' :
+		for i in range (0, len(controlSetinfo)) :
+			for j in range (0, len(usb_info[controlSetinfo[i]]["USBSTOR_Device_Name"])) :
+				if ((usb_info[controlSetinfo[i]]["USBSTOR_Device_Name"][j].upper() == events["Device_Name"]) and (usb_info[controlSetinfo[i]]["Serial_Number"][j].upper() == events["Serial_Number"])) :
+					csvWriter.writerow([list(usb_info.keys())[i], usb_info[controlSetinfo[i]]["Device_Name"][j], usb_info[controlSetinfo[i]]["Serial_Number"][j],
+					usb_info[controlSetinfo[i]]["Mounted_Volume_Name"][j], usb_info[controlSetinfo[i]]["Logical_Drive"][j], usb_info[controlSetinfo[i]]["Init Connect Time"][j],
+					events["Log_Time"], "", events["Lifetime"]])
+				else :
+					pass
+	elif events["Event_ID"] == '2100' :
+		for i in range (0, len(controlSetinfo)) :
+			for j in range (0, len(usb_info[controlSetinfo[i]]["USBSTOR_Device_Name"])) :
+				if ((usb_info[controlSetinfo[i]]["USBSTOR_Device_Name"][j].upper() == events["Device_Name"]) and (usb_info[controlSetinfo[i]]["Serial_Number"][j].upper() == events["Serial_Number"])) :
+					csvWriter.writerow([list(usb_info.keys())[i], usb_info[controlSetinfo[i]]["Device_Name"][j], usb_info[controlSetinfo[i]]["Serial_Number"][j],
+					usb_info[controlSetinfo[i]]["Mounted_Volume_Name"][j], usb_info[controlSetinfo[i]]["Logical_Drive"][j], usb_info[controlSetinfo[i]]["Init Connect Time"][j],
+					"", events["Log_Time"], events["Lifetime"]])
+				else :
+					pass
+	else :
+		pass
+
+def linkfile() :
+    # "C:\Users\<username>\AppData\Roaming\Microsoft\Windows\Start Menu" # 시작메뉴
+    # "C:\Users\<username>\AppData\Roaming\Microsoft\Windows\Recent" # 최근문서
+    # "C:\Users\<username>\Desktop" #바탕화면
+    # "C:\Users\<username>\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch" # 빠른실행
+    
+    for (path, dirs, files) in os.walk("C:/"):
+        for filename in files:
+            ext = os.path.splitext(filename)[-1]
+            if (ext == '.lnk') or (ext == '.LNK'):
+                lnkfile = "%s/%s" % (path, filename)
+                print (lnkfile)
+                print(parse_lnk(lnkfile))
+
+
 if __name__ == '__main__' :
-	global os_release, event_id_2003
+	global os_release, controlSetinfo
+	print ("OS System Info : ", end=" ");print (platform.platform())
 	os_release = platform.release()
-	event_id = list()
+	controlSetinfo = list()
 	D_classes()
+	for i in range (0, len(d_class_id)) :
+		controlSetinfo.append(d_class_id[i].split(" : ")[0])
+		controlSetinfo = list(set(controlSetinfo))
 	D_classes_1()
 	V_id_P_id()
 	mountDevice()
 	Volume_Deivce()
 	VolumeName()
 	setupdevlog()
-
-	print ("OS System Info : ", end=" ");print (platform.platform())
-	print ("\n================== * Registry Contents * ==================")
-	print ("Device Name : ", end=" "); print (d_class_id)
-	print ("USBSTOR Device Name : ", end=" "); print (stor_d_name)
-	print ("Serial_Number : ", end=" "); print (d_unique_instance_id)
-	print ("Vander & Product ID : ", end=" "); print (v_id_p_id)
-	print ("Logical Drive name : ", end=" "); print (logic_drive_name)
-	if os_release == "7" :
-		print ("Mount Volume Name : ", end=" "); print(win7_Volname)
-	elif os_release == "10" :
-		print ("Mount Volume Name : ", end=" "); print (win10_Volname)
-	else :
-		pass
-	print ("\n================== * Setupapi.dev.log Contents * ==================")
-	print ("Setupapi.dev.log : ", end=" "); print (search4log)
-	print ("Device Install Time : ", end=" "); print(initcontime)
-	
+	regi2dic()
 	Eventlogload()
-	# print ("EventID : ", end=" "); print(event_id_2003)
-
+	linkfile()
